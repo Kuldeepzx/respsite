115208d3e32a19a747e9fa7245a9b261a36eafee
patelcool <patelkuldeep349@gmail.com>
2019-03-28 04:43:12 -0800

    first commit

---
+++ scripts.js
@@ -0,0 +1,11 @@
+//Menu toggle open close karva mate
+$('.menu').on('click', function () {
+    $(this).toggleClass('active');
+    $('.overlay').toggleClass('menu-open');
+});
+
+$('.nav a').on('click', function () {
+    $('.menu').removeClass('active');
+    $('.overlay').removeClass('menu-open');
+
+});
